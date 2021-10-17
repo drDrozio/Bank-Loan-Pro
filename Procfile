0eed4db29cbdafa44f207509412904e5308b91be
drDrozio <ishanss2018@gmail.com>
2021-10-17 15:35:32 +0530

    For Heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-python manage.py collectstatic --noinput;
 web: gunicorn django_enviornment.wsgi
--- django_enviornment/settings.py
+++ django_enviornment/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join("./", 'staticfiles', "admin")
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static'), )
 
-print(STATIC_ROOT)
-#Our models folder 
-MODELS = os.path.join(BASE_DIR, 'main/model_tools')
+# print(STATIC_ROOT)
+# #Our models folder 
+# MODELS = os.path.join(BASE_DIR, 'main/model_tools')
